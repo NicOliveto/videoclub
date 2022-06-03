bcdb6cebbe53beb47d0d39185c272a8e38c9ac65
cwepfer <101940705+cwepfer@users.noreply.github.com>
2022-06-03 16:32:08 -0300

    Add files via upload

---
+++ VideoClub/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub
+{
+    public class Cliente : Persona
+
+    {
+        private string _idCliente;
+        private string _fechaAlta;
+        private bool _cliActivo;
+        private string _host;
+        private string _usuario;
+
+
+        public Cliente(string idCliente, string fechaAlta, bool cliActivo, string host, string usuario,
+            string nombre, string apellido, string direccion, int dni, string telefono, string mail, DateTime fechaNac )
+            : base (nombre, apellido, direccion, dni, telefono, mail, fechaNac)
+            
+            
+            {
+            _idCliente = idCliente;
+            _fechaAlta = fechaAlta;
+            _cliActivo = cliActivo;
+            _host = host;
+            _usuario = usuario;
+
+             }
+
+
+       
+
+    }
+
+
+
+
+
+
+    }
---
+++ VideoClub/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub
+{
+    public class Persona
+    {
+        protected string _nombre;
+        protected string _apellido;
+        protected string _direccion;
+        protected int _dni;
+        protected string _telefono;
+        protected string _mail;
+        protected DateTime _fechaNac;
+
+        public Persona(string nombre, string apellido, string direccion, int dni, string telefono, string mail, DateTime fechaNac)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _direccion = direccion;
+            _dni = dni;
+            _telefono = telefono;
+            _mail = mail;
+            _fechaNac = fechaNac;
+        }
+
+
+
+
+
+
+    }
+}
